9cbee9634ca6d73ed9f9bf819cbe33e791d82b8b
Fatima-1-B <fatimazahrabendoukali2@gmail.com>
2025-03-14 22:33:00 +0000

    j'ai modifier cheklistBox

--- Younes/Form1.Designer.cs
+++ Younes/Form1.Designer.cs
@@ -30,33 +30,34 @@
         {
             panel1 = new Panel();
             panelMenu = new Panel();
+            btnLogout = new Button();
+            btnTeam = new Button();
+            btnTasks = new Button();
+            btnProjects = new Button();
+            btnDashboard = new Button();
             panel2 = new Panel();
             panelMain = new Panel();
-            btnDashboard = new Button();
-            btnProjects = new Button();
-            btnTasks = new Button();
-            btnTeam = new Button();
-            btnLogout = new Button();
-            panelAdmin = new Panel();
-            panelChefProjet = new Panel();
-            panelChefEquipe = new Panel();
-            panel6 = new Panel();
             panelMembreEquipe = new Panel();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             label4 = new Label();
+            textBox4 = new TextBox();
+            panelChefEquipe = new Panel();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            panel6 = new Panel();
+            panelChefProjet = new Panel();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            panelAdmin = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            checkedListBox1 = new CheckedListBox();
             panel1.SuspendLayout();
             panelMenu.SuspendLayout();
             panelMain.SuspendLayout();
-            panelAdmin.SuspendLayout();
-            panelChefProjet.SuspendLayout();
-            panelChefEquipe.SuspendLayout();
             panelMembreEquipe.SuspendLayout();
+            panelChefEquipe.SuspendLayout();
+            panelChefProjet.SuspendLayout();
+            panelAdmin.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -65,8 +66,9 @@
             panel1.Controls.Add(panelMenu);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2, 2, 2, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(300, 863);
+            panel1.Size = new Size(150, 449);
             panel1.TabIndex = 0;
             // 
             // panelMenu
@@ -79,215 +81,246 @@
             panelMenu.Controls.Add(panel2);
             panelMenu.Dock = DockStyle.Fill;
             panelMenu.Location = new Point(0, 0);
+            panelMenu.Margin = new Padding(2, 2, 2, 2);
             panelMenu.Name = "panelMenu";
-            panelMenu.Size = new Size(300, 863);
+            panelMenu.Size = new Size(150, 449);
             panelMenu.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.Location = new Point(298, 7);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(1137, 867);
-            panel2.TabIndex = 0;
-            // 
-            // panelMain
-            // 
-            panelMain.Controls.Add(panelMembreEquipe);
-            panelMain.Controls.Add(panelChefEquipe);
-            panelMain.Controls.Add(panelChefProjet);
-            panelMain.Controls.Add(panelAdmin);
-            panelMain.Dock = DockStyle.Fill;
-            panelMain.Location = new Point(300, 0);
-            panelMain.Name = "panelMain";
-            panelMain.Size = new Size(1137, 863);
-            panelMain.TabIndex = 1;
-            // 
-            // btnDashboard
-            // 
-            btnDashboard.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btnDashboard.Location = new Point(0, 0);
-            btnDashboard.Name = "btnDashboard";
-            btnDashboard.Size = new Size(300, 69);
-            btnDashboard.TabIndex = 0;
-            btnDashboard.Text = "Tableau de Bord";
-            btnDashboard.UseVisualStyleBackColor = true;
-            // 
-            // btnProjects
-            // 
-            btnProjects.Location = new Point(0, 84);
-            btnProjects.Name = "btnProjects";
-            btnProjects.Size = new Size(297, 87);
-            btnProjects.TabIndex = 1;
-            btnProjects.Text = " Projets";
-            btnProjects.UseVisualStyleBackColor = true;
-            btnProjects.Click += button2_Click;
-            // 
-            // btnTasks
-            // 
-            btnTasks.Location = new Point(0, 177);
-            btnTasks.Name = "btnTasks";
-            btnTasks.Size = new Size(300, 84);
-            btnTasks.TabIndex = 2;
-            btnTasks.Text = " Tâches";
-            btnTasks.UseVisualStyleBackColor = true;
-            btnTasks.Click += button3_Click;
-            // 
-            // btnTeam
-            // 
-            btnTeam.Location = new Point(0, 281);
-            btnTeam.Name = "btnTeam";
-            btnTeam.Size = new Size(300, 82);
-            btnTeam.TabIndex = 3;
-            btnTeam.Text = "Équipe";
-            btnTeam.UseVisualStyleBackColor = true;
-            btnTeam.Click += button4_Click;
             // 
             // btnLogout
             // 
-            btnLogout.Location = new Point(0, 384);
+            btnLogout.Location = new Point(0, 200);
+            btnLogout.Margin = new Padding(2, 2, 2, 2);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(303, 83);
+            btnLogout.Size = new Size(152, 43);
             btnLogout.TabIndex = 4;
             btnLogout.Text = " Déconnexion";
             btnLogout.UseVisualStyleBackColor = true;
             btnLogout.Click += button5_Click;
             // 
-            // panelAdmin
+            // btnTeam
             // 
-            panelAdmin.Controls.Add(label1);
-            panelAdmin.Controls.Add(textBox1);
-            panelAdmin.Location = new Point(24, 32);
-            panelAdmin.Name = "panelAdmin";
-            panelAdmin.Size = new Size(219, 176);
-            panelAdmin.TabIndex = 0;
+            btnTeam.Location = new Point(0, 146);
+            btnTeam.Margin = new Padding(2, 2, 2, 2);
+            btnTeam.Name = "btnTeam";
+            btnTeam.Size = new Size(150, 43);
+            btnTeam.TabIndex = 3;
+            btnTeam.Text = "Équipe";
+            btnTeam.UseVisualStyleBackColor = true;
+            btnTeam.Click += button4_Click;
             // 
-            // panelChefProjet
+            // btnTasks
             // 
-            panelChefProjet.Controls.Add(label2);
-            panelChefProjet.Controls.Add(textBox2);
-            panelChefProjet.Location = new Point(280, 32);
-            panelChefProjet.Name = "panelChefProjet";
-            panelChefProjet.Size = new Size(236, 177);
-            panelChefProjet.TabIndex = 1;
-            panelChefProjet.Visible = false;
+            btnTasks.Location = new Point(0, 92);
+            btnTasks.Margin = new Padding(2, 2, 2, 2);
+            btnTasks.Name = "btnTasks";
+            btnTasks.Size = new Size(150, 44);
+            btnTasks.TabIndex = 2;
+            btnTasks.Text = " Tâches";
+            btnTasks.UseVisualStyleBackColor = true;
+            btnTasks.Click += button3_Click;
+            // 
+            // btnProjects
+            // 
+            btnProjects.Location = new Point(0, 44);
+            btnProjects.Margin = new Padding(2, 2, 2, 2);
+            btnProjects.Name = "btnProjects";
+            btnProjects.Size = new Size(148, 45);
+            btnProjects.TabIndex = 1;
+            btnProjects.Text = " Projets";
+            btnProjects.UseVisualStyleBackColor = true;
+            btnProjects.Click += button2_Click;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnDashboard.Location = new Point(0, 0);
+            btnDashboard.Margin = new Padding(2, 2, 2, 2);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(150, 36);
+            btnDashboard.TabIndex = 0;
+            btnDashboard.Text = "Tableau de Bord";
+            btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(149, 4);
+            panel2.Margin = new Padding(2, 2, 2, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(568, 452);
+            panel2.TabIndex = 0;
+            // 
+            // panelMain
+            // 
+            panelMain.Controls.Add(checkedListBox1);
+            panelMain.Controls.Add(panelMembreEquipe);
+            panelMain.Controls.Add(panelChefEquipe);
+            panelMain.Controls.Add(panelChefProjet);
+            panelMain.Controls.Add(panelAdmin);
+            panelMain.Dock = DockStyle.Fill;
+            panelMain.Location = new Point(150, 0);
+            panelMain.Margin = new Padding(2, 2, 2, 2);
+            panelMain.Name = "panelMain";
+            panelMain.Size = new Size(568, 449);
+            panelMain.TabIndex = 1;
+            // 
+            // panelMembreEquipe
+            // 
+            panelMembreEquipe.Controls.Add(label4);
+            panelMembreEquipe.Controls.Add(textBox4);
+            panelMembreEquipe.Location = new Point(428, 16);
+            panelMembreEquipe.Margin = new Padding(2, 2, 2, 2);
+            panelMembreEquipe.Name = "panelMembreEquipe";
+            panelMembreEquipe.Size = new Size(114, 92);
+            panelMembreEquipe.TabIndex = 3;
+            panelMembreEquipe.Visible = false;
+            panelMembreEquipe.Paint += panelMembreEquipe_Paint;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 20);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(153, 25);
+            label4.TabIndex = 1;
+            label4.Text = "Membre d’Équipe";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(14, 55);
+            textBox4.Margin = new Padding(2, 2, 2, 2);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(93, 31);
+            textBox4.TabIndex = 0;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
             // panelChefEquipe
             // 
             panelChefEquipe.Controls.Add(label3);
             panelChefEquipe.Controls.Add(textBox3);
             panelChefEquipe.Controls.Add(panel6);
-            panelChefEquipe.Location = new Point(590, 32);
+            panelChefEquipe.Location = new Point(295, 17);
+            panelChefEquipe.Margin = new Padding(2, 2, 2, 2);
             panelChefEquipe.Name = "panelChefEquipe";
-            panelChefEquipe.Size = new Size(225, 177);
+            panelChefEquipe.Size = new Size(112, 92);
             panelChefEquipe.TabIndex = 2;
             panelChefEquipe.Visible = false;
-            // 
-            // panel6
-            // 
-            panel6.Location = new Point(266, 0);
-            panel6.Name = "panel6";
-            panel6.Size = new Size(334, 300);
-            panel6.TabIndex = 0;
-            // 
-            // panelMembreEquipe
-            // 
-            panelMembreEquipe.Controls.Add(label4);
-            panelMembreEquipe.Controls.Add(textBox4);
-            panelMembreEquipe.Location = new Point(856, 31);
-            panelMembreEquipe.Name = "panelMembreEquipe";
-            panelMembreEquipe.Size = new Size(228, 176);
-            panelMembreEquipe.TabIndex = 3;
-            panelMembreEquipe.Visible = false;
-            panelMembreEquipe.Paint += panelMembreEquipe_Paint;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(13, 105);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(185, 55);
-            textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(41, 105);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(173, 55);
-            textBox2.TabIndex = 0;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(38, 105);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(168, 55);
-            textBox3.TabIndex = 1;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(28, 106);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(182, 55);
-            textBox4.TabIndex = 0;
-            textBox4.TextChanged += textBox4_TextChanged;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(46, 37);
-            label1.Name = "label1";
-            label1.Size = new Size(255, 48);
-            label1.TabIndex = 1;
-            label1.Text = "Administrateur";
-            label1.Click += label1_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(61, 37);
-            label2.Name = "label2";
-            label2.Size = new Size(246, 48);
-            label2.TabIndex = 1;
-            label2.Text = "Chef de Projet";
-            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(61, 37);
+            label3.Location = new Point(30, 19);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(239, 48);
+            label3.Size = new Size(122, 25);
             label3.TabIndex = 2;
             label3.Text = "Chef d’Équipe";
             // 
-            // label4
+            // textBox3
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(60, 38);
-            label4.Name = "label4";
-            label4.Size = new Size(302, 48);
-            label4.TabIndex = 1;
-            label4.Text = "Membre d’Équipe";
+            textBox3.Location = new Point(19, 55);
+            textBox3.Margin = new Padding(2, 2, 2, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(86, 31);
+            textBox3.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(133, 0);
+            panel6.Margin = new Padding(2, 2, 2, 2);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(167, 156);
+            panel6.TabIndex = 0;
+            // 
+            // panelChefProjet
+            // 
+            panelChefProjet.Controls.Add(label2);
+            panelChefProjet.Controls.Add(textBox2);
+            panelChefProjet.Location = new Point(140, 17);
+            panelChefProjet.Margin = new Padding(2, 2, 2, 2);
+            panelChefProjet.Name = "panelChefProjet";
+            panelChefProjet.Size = new Size(118, 92);
+            panelChefProjet.TabIndex = 1;
+            panelChefProjet.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 19);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Chef de Projet";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(20, 55);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(88, 31);
+            textBox2.TabIndex = 0;
+            // 
+            // panelAdmin
+            // 
+            panelAdmin.Controls.Add(label1);
+            panelAdmin.Controls.Add(textBox1);
+            panelAdmin.Location = new Point(12, 17);
+            panelAdmin.Margin = new Padding(2, 2, 2, 2);
+            panelAdmin.Name = "panelAdmin";
+            panelAdmin.Size = new Size(110, 92);
+            panelAdmin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 19);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Administrateur";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 55);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(94, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(355, 127);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(180, 144);
+            checkedListBox1.TabIndex = 4;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1437, 863);
+            ClientSize = new Size(718, 449);
             Controls.Add(panelMain);
             Controls.Add(panel1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             panel1.ResumeLayout(false);
             panelMenu.ResumeLayout(false);
             panelMain.ResumeLayout(false);
-            panelAdmin.ResumeLayout(false);
-            panelAdmin.PerformLayout();
-            panelChefProjet.ResumeLayout(false);
-            panelChefProjet.PerformLayout();
-            panelChefEquipe.ResumeLayout(false);
-            panelChefEquipe.PerformLayout();
             panelMembreEquipe.ResumeLayout(false);
             panelMembreEquipe.PerformLayout();
+            panelChefEquipe.ResumeLayout(false);
+            panelChefEquipe.PerformLayout();
+            panelChefProjet.ResumeLayout(false);
+            panelChefProjet.PerformLayout();
+            panelAdmin.ResumeLayout(false);
+            panelAdmin.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -315,5 +348,6 @@
         private Label label3;
         private Label label2;
         private Label label1;
+        private CheckedListBox checkedListBox1;
     }
 }
